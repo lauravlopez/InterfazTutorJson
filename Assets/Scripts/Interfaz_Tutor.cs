12e1ba20ab7071e8dadfa2a73bfa6d5a47ec80f1
lauravlopez <33065742+lauravlopez@users.noreply.github.com>
2019-04-06 23:32:50 -0500

    Change to another round if it's in the last position of the current round

--- Assets/Scripts/Interfaz_Tutor.cs
+++ Assets/Scripts/Interfaz_Tutor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Interfaz_Tutor : MonoBehaviour {
+
     public GameObject panelHola;
     public GameObject panel;
     public Button openTutorButton;
@@ -18,31 +19,27 @@ public class Interfaz_Tutor : MonoBehaviour {
     public Text messageText;
     private MessageDataController messageDataController;
     private MessageRoundData currentMessageRoundData;
-    private MessageData Datas;
     public MessageData[] messagePool;
-
+    public GameData Rounds;
     private bool isRoundedActive;
     public int messageIndex = 0;
-    private int nextRound = 0;
-
-    //public List<Sprite> pantallaSprites = new List<Sprite>();
-    //public Sprite[] pantallaSprites;
-   // int pos=0;
-
+    public int nextRound = 0;
+    public int LenRounds = 0;
 
     public void Start () {
+        messageIndex = 0;
+        nextRound = 0;
         closeTutorButton.onClick.AddListener(CloseTutor);
         openTutorButton.onClick.AddListener(OpenPanel);
-       // UIImage = GameObject.Find("ImgNext").GetComponent<Image>();
+        panel.gameObject.SetActive(true);
         closeTutorButton.gameObject.SetActive(false);
         panelHola.gameObject.SetActive(false);
-        Next.gameObject.SetActive(false);
-        Previous.gameObject.SetActive(false);
+       
         Correct.gameObject.SetActive(false);
         Incorrect.gameObject.SetActive(false);
         UIImage.gameObject.SetActive(false);
         ExitPanel.gameObject.SetActive(false);
-
+       
         messageDataController = FindObjectOfType<MessageDataController>();
         currentMessageRoundData = messageDataController.GetCurrentMessageRoundData(0);
         messagePool = currentMessageRoundData.messages;
@@ -56,6 +53,7 @@ public class Interfaz_Tutor : MonoBehaviour {
 
         MessageData messageData = messagePool[messageIndex];
         messageText.text = messageData.messageText;
+       
 
     }
     private void ViewImage()
@@ -67,12 +65,29 @@ public class Interfaz_Tutor : MonoBehaviour {
     }
 
     public void OpenPanel() {
+        Conditions();
         openTutorButton.gameObject.SetActive(false);
         closeTutorButton.gameObject.SetActive(true);
         UIImage.gameObject.SetActive(true);
-        //Debug.Log(pantallaSprites[pos]);
         panelHola.gameObject.SetActive(true);
-        Conditions();
+
+        if ((messagePool[messageIndex].GoodStateButton == true) && (messagePool[messageIndex].ExitButton == true))
+        {
+            Incorrect.gameObject.SetActive(true);
+            Correct.gameObject.SetActive(true);
+            Next.gameObject.SetActive(false);
+            Previous.gameObject.SetActive(false);
+        }
+        else if (!(Next.gameObject.activeSelf) && !(Previous.gameObject.activeSelf))
+        {
+            Next.gameObject.SetActive(true);
+            Previous.gameObject.SetActive(true);
+        }
+
+        //Si incorrecto y correct estan activos. Dejar activo.
+
+        
+
     }
 
     public void CloseTutor()
@@ -84,8 +99,10 @@ public class Interfaz_Tutor : MonoBehaviour {
         Previous.gameObject.SetActive(false);
         UIImage.gameObject.SetActive(false);
         Correct.gameObject.SetActive(false);
-        Incorrect.gameObject.SetActive(false);
         ExitPanel.gameObject.SetActive(false);
+        Incorrect.gameObject.SetActive(false);
+        Correct.gameObject.SetActive(false);
+       
     }
 
     public void NextImage()
@@ -94,19 +111,10 @@ public class Interfaz_Tutor : MonoBehaviour {
       if (messageIndex != messagePool.Length - 1)
         {
             messageIndex = messageIndex + 1;
-            int n = messagePool.Length;
-            Debug.Log("mi piscina es de posiciones " + n);
-            Debug.Log("estoy en la posicion" + messageIndex);
+
             if ((messagePool[messageIndex].GoodStateButton == true) && (messagePool[messageIndex].ExitButton == true))
                     {
                        
-
-                        bool good = messagePool[messageIndex].GoodStateButton;
-                        bool bad = messagePool[messageIndex].ExitButton;
-                        string men = messagePool[messageIndex].messageText;
-
-                        Debug.Log("el estado bueno es " + good + " el estado malo es " + bad + " y el mensaje es :" + men);
-
                         MessageData messageData = messagePool[messageIndex];
 
                         messageText.text = messageData.messageText;
@@ -114,58 +122,37 @@ public class Interfaz_Tutor : MonoBehaviour {
                         Incorrect.gameObject.SetActive(true);
                         Next.gameObject.SetActive(false);
                         Previous.gameObject.SetActive(false);
-                        Debug.Log(messageIndex + "mensaje index");
-                        Debug.Log("estoy en el primer else");
                         CorrectState();
                     }
                    else if ((messagePool[messageIndex].GoodStateButton == false) && (messagePool[messageIndex].ExitButton == false))
                         {
                             
-                            //Debug.Log("el estado bueno es " + good + " el estado malo es " + bad + " y el mensaje es :" + men);
                             MessageData messageData = messagePool[messageIndex];
                             messageText.text = messageData.messageText;
                             Previous.gameObject.SetActive(true);
-                            Debug.Log(messageIndex + "mensaje index");
-                            Debug.Log("estoy en el segundo else");
                         }
             }
         else
             {
-               // NextRound();
+                NextRound();  //Si es el ultimo, pasa al siguiente Round
             }
        
     }
-    /*
-    else if (messageIndex == messagePool.Length - 1)
-
-    if (messageIndex == 2) {
-        Next.gameObject.SetActive(false);
-        Previous.gameObject.SetActive(false);
-        Correct.gameObject.SetActive(true);
-        Incorrect.gameObject.SetActive(true);
-    }*/
-
 
     public void PreviousImage()
         {
             messageIndex = messageIndex - 1;
             MessageData messageData = messagePool[messageIndex];
             messageText.text = messageData.messageText;
-            /* pos = pos - 1;
-             Sprite pantallaSprite = pantallaSprites[pos];
-             UIImage.sprite = pantallaSprite;
-             Debug.Log(pantallaSprites[pos]);*/
-             Next.gameObject.SetActive(true);
-             Conditions();
+            Next.gameObject.SetActive(true);
+            Conditions();
             //CorrectState();
         }
 
    public void CorrectState()
  
-        {         
-          
-           
-            closeTutorButton.onClick.AddListener(NextRound);
+        {   
+            //closeTutorButton.onClick.AddListener(NextRound);
             Correct.onClick.AddListener(Exit);
             Incorrect.onClick.AddListener(IncorrectState);
         } 
@@ -173,21 +160,24 @@ public class Interfaz_Tutor : MonoBehaviour {
 
     public void IncorrectState()
         {
-        //------------- SI ESTA EN LA ULTIMA POS, NEXT ROUND..
-        if (messageIndex == messagePool.Length - 1)
-            {
-                messageIndex = 0;
-            }
-        else
-            {
-            // messageIndex = messageIndex + 1;
-            
-            NextRound();
             Next.gameObject.SetActive(true);
-                Previous.gameObject.SetActive(false);
-                Correct.gameObject.SetActive(false);
-                Incorrect.gameObject.SetActive(false);
-            }
+            Previous.gameObject.SetActive(false);
+            Correct.gameObject.SetActive(false);
+            Incorrect.gameObject.SetActive(false);
+
+        if (messageIndex == messagePool.Length - 1)
+                {
+                    NextRound();
+                   
+                }
+           
+             else if (messageIndex == 5)
+                {
+                    NextImage();
+                }
+                
+               
+        
         }
 
     public void Exit()
@@ -196,43 +186,31 @@ public class Interfaz_Tutor : MonoBehaviour {
             Previous.gameObject.SetActive(false);
             Correct.gameObject.SetActive(false);
             Incorrect.gameObject.SetActive(false);
-            closeTutorButton.onClick.AddListener(NextRound);
+            closeTutorButton.onClick.AddListener(Start);
             ExitPanel.gameObject.SetActive(true);
         }
 
     private void NextRound()
         {
-
-
             messageIndex = 0;
             nextRound = nextRound + 1;
             currentMessageRoundData = messageDataController.GetCurrentMessageRoundData(nextRound); //get position n
             messagePool = currentMessageRoundData.messages;
-           
             MessageData messageData = messagePool[messageIndex];
-            messageText.text = messageData.messageText;
-            //Sprite pantallaSprite = pantallaSprites[pos];
-            //UIImage.sprite = pantallaSprite;
+            messageText.text = messageData.messageText;  
         }
-
-
-   
-    /*
-    private void state(MessageData m)
+    private void Reset()
     {
-        if (states == true)
-        {
-            Next.gameObject.SetActive(false);
-            Previous.gameObject.SetActive(false);
-            Correct.gameObject.SetActive(true);
-            Incorrect.gameObject.SetActive(true);
-            Debug.Log("el estado es true");
-        }
-    }*/
+        messageIndex = 0;
+        currentMessageRoundData = messageDataController.GetCurrentMessageRoundData(0); //get position 0
+        messagePool = currentMessageRoundData.messages;
+        MessageData messageData = messagePool[0];
+        messageText.text = messageData.messageText;
+    }
 
     private void Conditions()
     {
-        if (messageIndex == 0 /*|| messageIndex == 4*/)
+        if (messageIndex == 0 )
         {
 
             Next.gameObject.SetActive(true);
@@ -240,23 +218,5 @@ public class Interfaz_Tutor : MonoBehaviour {
             Correct.gameObject.SetActive(false);
             Incorrect.gameObject.SetActive(false);
         }
-        /*
-        else
-        {
-            if (messageIndex == 2)
-            {
-                Next.gameObject.SetActive(false);
-                Previous.gameObject.SetActive(false);
-                Correct.gameObject.SetActive(true);
-                Incorrect.gameObject.SetActive(true);
-            }
-            else
-            {
-                Next.gameObject.SetActive(true);
-                Previous.gameObject.SetActive(true);
-                Correct.gameObject.SetActive(false);
-                Incorrect.gameObject.SetActive(false);
-            }
-        }*/
     }
 }
